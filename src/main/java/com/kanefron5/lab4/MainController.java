894a184ae751dd68c34f9c862f26224b9d836d15
kanefron5 <kanefron5@gmail.com>
2020-01-21 20:30:20 +0300

    фиксы

--- src/main/java/com/kanefron5/lab4/MainController.java
+++ src/main/java/com/kanefron5/lab4/MainController.java
@@ -10,15 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.annotation.ApplicationScope;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
 @Controller
-@ApplicationScope
-@CrossOrigin
 @RequestMapping(value = "/api/*")
 public class MainController {
     private final static String error_f = "{\"error\":\"%s\"}";
